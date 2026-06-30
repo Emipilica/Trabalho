5a30ec11ffe73d227639a0b3823a8ba652026006
Emipilica <capir.2025116isinf0031@aluno.ifpi.edu.br>
2026-06-30 14:26:31 -0300

    Change success message in form submission
    Updated success message in form submission handler.

--- home/script.js
+++ home/script.js
@@ -5,6 +5,6 @@ formulario.addEventListener("submit", function(event){
     event.preventDefault();
     mensagem.style.display = "block";
     mensagem.innerHTML =
-    "🌸 Sua solicitação foi enviada com sucesso! Em breve começaremos a transformar sua história em uma carta inesquecível.";
+"✉ Solicitação enviada com sucesso!<br><br>Obrigado por confiar sua história à Cartas & Memórias. Em breve nossa equipe entrará em contato para iniciar a escrita da sua carta.";
     formulario.reset();
 });
